0c497b4b76e123496de7984ddf96514aea239a83
wujianfeng01 <2021224015@chd.edu.cn>
2023-08-18 21:07:55 +0800

    自定义注解

---
+++ src/Annotation/ValueInfo.java
@@ -0,0 +1,16 @@
+package Annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 定义自己的注解 @ValueInfo
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ValueInfo {
+    String name();
+    int age();
+}
